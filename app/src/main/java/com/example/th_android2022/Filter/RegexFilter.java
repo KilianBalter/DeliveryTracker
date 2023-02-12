87859b68e7a3e57b056c7cd95e4cf7a531dacf6e
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2023-02-12 15:45:39 +0100

    Fix regex

--- app/src/main/java/com/example/th_android2022/Filter/RegexFilter.java
+++ app/src/main/java/com/example/th_android2022/Filter/RegexFilter.java
@@ -5,8 +5,8 @@ import com.example.th_android2022.Entities.Email;
 import java.util.regex.Pattern;
 
 public class RegexFilter {
-    private final static Pattern PACKAGE_PATTERN_EN = Pattern.compile("(package|deliver|delivered|track|ship|arrive|arriving|order|dispatched)", Pattern.CASE_INSENSITIVE);
-    private final static Pattern PACKAGE_PATTERN_DE = Pattern.compile("(paket|lieferung|zustellung|zugestellt|geliefert|verfolge|verfolgung|unterwegs|auf dem weg|ankommen|angekommen|bestellung|bestellt|versand)", Pattern.CASE_INSENSITIVE);
+    private final static Pattern PACKAGE_PATTERN_EN = Pattern.compile("package|delivery|deliver|delivered|track|tracking|ship|shipping|arrive|arriving|order|dispatched", Pattern.CASE_INSENSITIVE);
+    private final static Pattern PACKAGE_PATTERN_DE = Pattern.compile("paket|lieferung|zustellung|zugestellt|geliefert|verfolge|verfolgung|unterwegs|auf dem weg|ankommen|angekommen|bestellung|bestellt|versand", Pattern.CASE_INSENSITIVE);
 
     public static boolean filter(Email email) {
         String full = email.getSubject() + email.getContent();
