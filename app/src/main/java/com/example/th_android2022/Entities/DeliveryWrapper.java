1b88d51497d84f028f01400cde360d38166b394c
Henrik <henrik.siesenop@th-bingen.de>
2023-02-14 10:43:00 +0100

    updated Delivery Wrapper

--- app/src/main/java/com/example/th_android2022/Entities/DeliveryWrapper.java
+++ app/src/main/java/com/example/th_android2022/Entities/DeliveryWrapper.java
@@ -6,26 +6,49 @@ import androidx.annotation.NonNull;
 
 import com.example.th_android2022.Databases.DeliveryDAO;
 
+import java.util.List;
 import java.util.regex.Pattern;
+
+import io.realm.RealmList;
 
 public class DeliveryWrapper {
     private static final Pattern ORDER_ID_PATTERN = Pattern.compile("(\\d|-)+");
     private static final String[] ORDER_ID_PREFIXES = {"Sendungsnummer ", "Sendungsnummer: ", "Sendung ", "Bestellung ", "Bestellnr. ", "Bestellung Nr. "};
 
-    public static void extractData(@NonNull Email email, Context appContext){
+
+  public static void extractData(@NonNull Email email, Context appContext){
         DeliveryDAO DAO = new DeliveryDAO(appContext);
+        Delivery d;
         String content = email.getContent().toLowerCase();
-        String orderID = findOrderID(content);
-        //If no orderID is found in the email content, try to find it in the subject
-        if (orderID == null) {
+        String orderID = findOrderID(email.getTrackingLink());
+        if (orderID == null) {                                                          //If no orderID is found in the tracking link, try to find it in the subject
             orderID = findOrderID(email.getSubject());
+        }
+        if (orderID == null) {
+            orderID = findOrderID(content);
         }
         String deliveryService = findDeliveryService(email);
         String tag = findTag(email);
-        Delivery d = new Delivery(tag, Delivery.Status.ACTIVE, orderID, deliveryService);
-        d.addEmail(email);
-
-        DAO.insertOnlySingleDelivery(d);
+        if (orderID == null || deliveryService == null) {
+            d = new Delivery(tag, Delivery.Status.ACTIVE, orderID, deliveryService);
+            d.addEmail(email);
+            DAO.insertOnlySingleDelivery(d);
+        }
+        else{
+            d = DAO.findFirstByOrderIdAndDeliveryService(orderID, deliveryService);
+            if (d == null){
+                d = new Delivery(tag, Delivery.Status.ACTIVE, orderID, deliveryService);
+                d.addEmail(email);
+                DAO.insertOnlySingleDelivery(d);
+            }
+            else {
+                List<Email> EmailList = d.getEmailList();
+                EmailList.add(email);
+                d.setEmailList(EmailList);
+                d.setTag(tag);
+                DAO.updateOnlySingleDelivery(d);
+            }
+        }
     }
 
     private static String findOrderID(String content) {
@@ -49,7 +72,6 @@ public class DeliveryWrapper {
 
             i++;
         }
-
         return orderID;
     }
 
